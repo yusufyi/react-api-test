12c8530ab93554beb4539de5279dc78ed58cec01
Ryan Holder <ryanh@wearedauntless.com>
2021-09-29 15:21:25 +0100

    init: Initialise the react test repo

---
+++ README.md
@@ -0,0 +1,73 @@
+# Dauntless React Test
+
+## Overview
+
+To complete this assessment, you will need to write a simple React web app, and provide us the source files to be built.
+
+The purpose of this assessment is to assess your skills and approach to composing a simple web app given an API feed. We will also assess the generated HTML, CSS, and JS output.
+
+This assessment is expected to take about 1-2 hours.
+
+---
+
+## How to use
+
+Download or clone this [repo](https://github.com/wearedauntless)
+
+Install it and run:
+
+```sh
+npm install
+npm start
+```
+
+---
+
+## The idea behind the test
+
+In the source code of this test we have a basic template that makes use of [Create React App](https://github.com/facebookincubator/create-react-app) and `@mui/material` and its peer dependencies, including `emotion`, the default style engine in MUI v5.
+
+The template is a page that is ready to display images, titles and information in a material-ui card style. In the code that is provided to you it will render the application with 9 cards of dummy information which it generates based on the basic array called `cards` that lives within the `App.js` file. The images for each card are called from and random image generator and the card title and details are "hard coded".
+
+You tasks will be to turn this static content in to a dynamic feed of crypto currencies that are traded against the dollar (USD). More detailed steps on the application requirement are set out below.
+
+For this test you will be making use of the the **free** [CoinGecko API](https://www.coingecko.com/en/api/documentation). Keep in mind when building that their free API has a rate limit of 50 calls/minute, however this should be more than sufficient.
+
+---
+
+## Application requirement
+
+- Access the CoinGecko api using the `/coins/markets` endpoint and return 45 results using `USD` for the `vs_currency` parameter
+
+- With the returned data you will need to display the 45 results with the following information on the cards;
+
+  - Image
+  - Currency Name
+  - Current Price
+  - 24h High Price
+  - 24H Low Price
+
+- The cards currently have a "MORE" button that is not connected to any routes. This button needs to take the user to a route for the clicked on currency with the path following the following format `/currency/{symbol}` where `{symbol}` is a value from the returned data.
+
+- Once the user lands on the `/currency/{symbol}` page for the selected crypto currency you need to display the following information;
+  - Currency Name
+  - Current Price
+  - All time high price
+  - Market Cap
+  - Market Cap Rank
+
+---
+
+## Additional items to consider
+
+- Ensure you have navigation back from the currency specific pages back to the main list page.
+- The code provided has no code or libraries related to data fetching and this is the main focus of what you are implementing. For this test we do not require that you use a specific library but would recommend [React Query](https://react-query.tanstack.com/) as this is our preferred option.
+- We want to see you use React function components
+- Making use of React Hooks where and if it may make sense would show us your understanding of this.
+- If you would like to showcase your understanding of component styling then feel inspired to modify the basics that have been provided to you.
+
+---
+
+## Steps to complete your project
+
+- At the start of this project you would have forked the Dauntless React Test repository and once you are complete we would like you to push the code to a public repository in your github account and then provide the link to the repository to Dauntless.
---
+++ src/App.js
@@ -0,0 +1,118 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
+
+export default function Album() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <BarChartIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            CoinGecko Market Pairs (USD)
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Market Pairs (USD)
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              The following is a list of crypto currencies with information
+              related to the USD trading pair.
+            </Typography>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Currency Name
+                    </Typography>
+                    <Typography>
+                      <ul>
+                        <li>Current Price: xxx</li>
+                        <li>24h High: xxx</li>
+                        <li>24h Low: xxx</li>
+                      </ul>
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">More</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
